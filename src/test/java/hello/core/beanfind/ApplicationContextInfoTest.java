b539be802eca66ff2c7dadceec17d35ebcd235ba
jun1116 <52767291+jun1116@users.noreply.github.com>
2021-08-13 22:17:21 +0900

    스프링컨테이너Test_getBeanDefinitionNames

---
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,38 @@
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+//import org.springframework.context.ApplicationContext;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+class ApplicationContextInfoTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("모든빈출력하기")
+    void findAllBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            Object bean = ac.getBean(beanDefinitionName);
+            System.out.println("name = " + beanDefinitionName + " Object = "+bean);
+        }
+    }
+    @Test
+    @DisplayName("Print User App BEAN")
+    void findApplicationBean(){
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+            /*
+            BeanDefinition.ROLE_APPLICATION : 개발자가 직접 등록한 App Bean
+            BeanDefinition.ROLE_INFRASTRUCTURE : Spring 내부에서 사용하는 App Bean
+            * */
+            if(beanDefinition.getRole()==BeanDefinition.ROLE_APPLICATION){
+                Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name = " + beanDefinitionName + " Object = "+bean);
+            }
+        }
+    }
+}
