793dcb2ae177f66f60a4f3c288c44c2d425f9a80
hj <jun1057@ajou.ac.kr>
2021-08-16 01:17:18 +0900

    상속관계

--- src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
@@ -3,6 +3,7 @@ package hello.core.beanfind;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import javafx.beans.DefaultProperty;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -51,6 +52,17 @@ public class ApplicationContextSameBeanFindTest {
         assertThat(beansOfType.size()).isEqualTo(2);
     }
 
+    @Test
+    @DisplayName("특정 타입을 모두 조회하기")
+    void findAllBeanByType_2(){
+        Map<String, MemberRepository> beansOfType = ac.getBeansOfType(MemberRepository.class);
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key = " + key + "Value = " + beansOfType.get(key));
+            System.out.println("beansOfType = " + beansOfType);
+            assertThat(beansOfType.size()).isEqualTo(3);
+        }
+    }
+
     @Configuration
     static class SameBeanConfig {
         @Bean
@@ -60,6 +72,11 @@ public class ApplicationContextSameBeanFindTest {
 
         @Bean
         public MemberRepository memberRepository2() {
+            return new MemoryMemberRepository();
+        }
+
+        @Bean
+        public MemberRepository memberRepository3(){
             return new MemoryMemberRepository();
         }
     }
