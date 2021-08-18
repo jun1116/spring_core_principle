d2f94154dd55a258d2896a9b2e6c73d51dbcf7ba
jun1116 <52767291+jun1116@users.noreply.github.com>
2021-08-19 00:38:03 +0900

    DI 필드주입방식 테스트

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -35,8 +35,10 @@ public class AppConfig {
     }
     @Bean
     public OrderService orderService(){
-        return new OrderServiceImpl( memberRepository(), discountPolicy() );
+//        return new OrderServiceImpl( memberRepository(), discountPolicy() );
+        return null;
     }
+
     @Bean
     public DiscountPolicy discountPolicy(){
 //        return new FixDiscountPolicy();
--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
@@ -8,4 +11,8 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 @ComponentScan(excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
 public class AutoAppConfig {
+//    @Bean(name = "memoryMemberRepository")
+//    public MemberRepository memberRepository(){
+//        return new MemoryMemberRepository();
+//    }
 }
