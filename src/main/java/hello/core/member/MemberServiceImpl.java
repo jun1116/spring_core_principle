170eeae7af00b44dbc8d40597b206878c0101175
jun1116 <52767291+jun1116@users.noreply.github.com>
2021-08-18 21:24:03 +0900

    winsave 컴포넌트스캔, 탐색위치와 기본스캔대상까지

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,5 +1,9 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService {
     //    앞에있는건 인터페이스 , 뒤에있는건 구현체
     // 그 이유는 memberRepository가 Null Point Exception이 터져서
@@ -11,9 +15,11 @@ public class MemberServiceImpl implements MemberService {
     그것은 오직 외부(AppConfig)에서 결정
     의존관계는 외부에 맡기고, 실행에만 집중하면됨
     */
+    //@Autowired //생성자가 딱 하나있는 Component는 자동으로 Autowired가 붙어있다. -> MemberRepository를 스프링이 자동으로 찾아서 붙여준다
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
+    //ac.getBean(MemberRepository.class); 의 기능을 스프링이 Autowired를 보고 대신해줘
 
     @Override
     public void join(Member member) {
