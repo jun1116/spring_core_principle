b31912bc56be8df42a0ce08339bd2630f26bab6f
hj <jun1057@ajou.ac.kr>
2021-08-23 22:13:21 +0900

    실행해보면 ac.getBean() 을 통해서 항상 새로운 프로토타입 빈이 생성되는 것을 확인할 수 있다. 의존관계를 외부에서 주입(DI)받는게 아니라, 이렇게 직접 필요한 의존관계를 찾는 것을 Dependency Lookup (DL) 의존관계 조회(탐색) 이라 한다.

--- src/test/java/hello/core/scope/PrototypeProviderTest.java
+++ src/test/java/hello/core/scope/PrototypeProviderTest.java
@@ -1,33 +1,34 @@
 package hello.core.scope;
-import org.junit.jupiter.api.DisplayName;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class SingletonWithPrototypeTest1 {
+public class PrototypeProviderTest {
     @Test
-    void providerTest(){
-        ApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
+    void SingletonClientUsePrototype(){
+        ApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class,PrototypeBean.class);
+
         ClientBean clientBean1 = ac.getBean(ClientBean.class);
         int count1 = clientBean1.logic();
         assertThat(count1).isEqualTo(1);
-        System.out.println("count1 = " + count1);
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(1);
-        System.out.println("count2 = " + count2);
+        assertThat(count2).isEqualTo(2);
     }
     static class ClientBean{
+        private final PrototypeBean prototypeBean;
         @Autowired
-        private ApplicationContext ac;
+        public ClientBean(PrototypeBean prototypeBean) {this.prototypeBean = prototypeBean;}
         public int logic(){// 더하고, 꺼내서 반환
-            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
