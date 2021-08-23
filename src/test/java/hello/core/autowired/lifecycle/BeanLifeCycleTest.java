c2950e0c8c704fb92039de31c3df115564ac7401
hj <jun1057@ajou.ac.kr>
2021-08-23 21:17:58 +0900

    싱글톤 빈, 프로토타입 빈 테스트 싱글톤빈 : 스프링컨테이너 생성시점에 초기화매서드호출 프로토타입 : 요청시 생성, 종료메서드 호출되지않음, -> 클라이언트가 관리해야해

--- src/test/java/hello/core/autowired/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/autowired/lifecycle/BeanLifeCycleTest.java
@@ -5,21 +5,34 @@ import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 public class BeanLifeCycleTest {
     @Test
     public void LifeCycleTest(){
         ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
         NetworkClient client = ac.getBean(NetworkClient.class);
-        ac.close();
-    }
+        ac.close();}
+
     @Configuration
-    static class LifeCycleConfig{
+    static class LifeCycleConfig {
         @Bean
-        public NetworkClient networkClient(){
+        public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("IN CONFIG :: Spring__url.rurlrlrrjfl");
             return networkClient;
         }
+
+//        @Scope("prototype")
+//        @Component
+//        public class HelloBean {
+//        }
+//
+//        @Scope("prototype")
+//        @Bean
+//        PrototypeBean HelloBean(){
+//            return new HelloBean();
+//        }
     }
 }
